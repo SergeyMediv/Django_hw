69982fab388c1f8724d7cd88964c2a04f651aef3
mediv <mediv@mail.ru>
2024-03-13 21:05:25 +0300

    make categories and database settings

--- catalog/models.py
+++ catalog/models.py
@@ -24,3 +24,11 @@ class Product(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
