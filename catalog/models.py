2ae858186b8fe62f1afc8a3ad77e1e5cafe9c870
mediv <mediv@mail.ru>
2024-03-13 21:55:16 +0300

    create and undo migration

--- catalog/models.py
+++ catalog/models.py
@@ -20,11 +20,11 @@ class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    # category = models.IntegerField(verbose_name='Категория')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена')
     created_at = models.DateField(verbose_name='Дата создания')
     updated_at = models.DateField(verbose_name='Дата изменения')
+    manufactured_at = models.DateField(verbose_name='Дата производства', **NULLABLE)
 
     def __str__(self):
         return f"{self.name}"
@@ -33,4 +33,3 @@ class Product(models.Model):
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         ordering = ('name',)
-
