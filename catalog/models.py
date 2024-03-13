8a6478e57f96d3f2cbac67e7e91e18d416834867
mediv <mediv@mail.ru>
2024-03-13 21:59:14 +0300

    remove manufactured at

--- catalog/models.py
+++ catalog/models.py
@@ -24,7 +24,6 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена')
     created_at = models.DateField(verbose_name='Дата создания')
     updated_at = models.DateField(verbose_name='Дата изменения')
-    manufactured_at = models.DateField(verbose_name='Дата производства', **NULLABLE)
 
     def __str__(self):
         return f"{self.name}"
