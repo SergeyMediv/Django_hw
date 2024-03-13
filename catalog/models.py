5cd1743bb7421c45d991b3d4776c486e692addd7
mediv <mediv@mail.ru>
2024-03-13 21:45:37 +0300

    change categories + create foreign key

--- catalog/models.py
+++ catalog/models.py
@@ -3,24 +3,6 @@ from django.db import models
 NULLABLE = {'null': True, 'blank': True}
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Наименование')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
-    category = models.IntegerField(verbose_name='Категория')
-    price = models.IntegerField(verbose_name='Цена')
-    create_date = models.DateField(verbose_name='Дата создания')
-    change_date = models.DateField(verbose_name='Дата изменения')
-
-    def __str__(self):
-        return f"{self.name}"
-
-    class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
-        ordering = ('name',)
-
-
 class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
@@ -32,3 +14,23 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    # category = models.IntegerField(verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('name',)
+
