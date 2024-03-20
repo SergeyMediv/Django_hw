426f95f13af121160d8563df9ee1a6268ea157b8
mediv <mediv@mail.ru>
2024-03-21 00:22:11 +0300

    product card added

--- catalog/views.py
+++ catalog/views.py
@@ -12,9 +12,21 @@ def home(request):
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name}\n{phone}\n{message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
+
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'object': product,
+        'title': product.name,
+    }
+    return render(request, 'catalog/card.html', context)
