4584000270364f5551580f0817a7cd025eabb3e8
mediv <mediv@mail.ru>
2024-03-21 20:45:15 +0300

    main menu added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from catalog.views import home, contacts, product_info
+from catalog.views import home, contacts, product_info, category_product, main_menu
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
     path('catalog/<int:pk>/', product_info, name='product_info'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, )
+    path('catalog/category_product/<int:pk>/', category_product, name='category_product'),
+    path('main_menu', main_menu, name='main_menu')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
