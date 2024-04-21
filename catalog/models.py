ad48fc13ec6b1dbbb2685999afa5081b6328c87b
mediv <mediv@mail.ru>
2024-04-21 20:57:17 +0300

    owner field added

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 NULLABLE = {'null': True, 'blank': True}
@@ -24,6 +25,7 @@ class Product(models.Model):
     price = models.PositiveIntegerField(verbose_name='Цена')
     created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
     updated_at = models.DateField(verbose_name='Дата изменения', auto_now=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Автор')
 
     def __str__(self):
         return f"{self.name} ({self.category})"
